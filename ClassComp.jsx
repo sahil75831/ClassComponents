4fadaf605b9337d4bf6aaf5350ab9c18d1a7086f
sahilsharma <sahilsharma75831@gmail.com>
2023-07-19 15:08:34 +0530

    first commit

--- ClassComp.jsx
+++ ClassComp.jsx
@@ -5,41 +5,42 @@ class CarParent extends React.Component {
     return (
       <>
         <h1> car parent component</h1>
-        <i>following is the example of props in component is the</i>
-        <Car role="frontend" />
+        <i>following is the example of state change in component </i>
+        <Car />
       </>
     );
   }
 }
 
-// If your component has a constructor function, the props should always be passed to the constructor and also to the React.Component via the super() method.
 
 class Car extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "sahil",
-      profile: "web developer",
+        brand: "Ford",
+        model: "Mustang",
+        color: "red",
+        year: 1964
     };
   }
+
+//  To change a value in the state object, use the this.setState() method.
+// When a value in the state object changes, the component will re-render, meaning that the output will change according to the new value(s).
+
+  handleClick = () => {
+    this.setState({color:"blue", year:this.state.year+1 })
+  }
+
   render() {
     return (
       <>
-        <h2>Hi i'm {this.state.name}</h2>
-        <i>i am a {this.props.role} {this.state.profile}</i>
+        <h2>Hi i'm {this.state.brand}</h2>
+        model : {this.state.model}
+        <br/>color : {this.state.color}
+        <br/>year : {this.state.year}
+        <br/><button onClick={this.handleClick}>change color</button>
       </>
     );
   }
 }
 export default CarParent;
-
-// If there is a constructor() function in your component, this function will be called when the component gets initiated.The constructor function is where you initiate the component's properties.
-
-// The constructor function is also where you "honor the inheritance of the parent component by including the super() statement" , which executes the parent component's constructor function, and your component has access to all the functions of the parent component (React.Component).
-
-
-// OUTPUT
-// car parent component
-// following is the example of props in component is the
-// Hi i'm sahil
-// i am a frontend web developer
