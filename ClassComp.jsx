23d7d1276bbf489c946a0c178d3fc076df2700d4
sahilsharma <sahilsharma75831@gmail.com>
2023-07-19 16:47:40 +0530

    first commit

--- ClassComp.jsx
+++ ClassComp.jsx
@@ -12,39 +12,38 @@ class CarParent extends React.Component {
   }
 }
 
-
 class Car extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        brand: "Ford",
-        model: "Mustang",
-        color: "red",
-        year: 1964
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964,
     };
-  }
-  
-  static getDerivedStateFromProps(props, state) {
-    return {color: props.favColor, brand:"maruti" };
 }
+// note: Lifecycle method should be static: getDerivedStateFromProps
+// static getDerivedStateFromProps(props, state) {
+//     console.log("getDerivedStateFromProps")
+//     return { color: props.favColor, brand: "maruti" };
+//   }
 
+  componentDidMount(){
+    // note : don't use getDerivedStateFromProps otherwise it wont show any change because as soon componentdidmount fires state change due to which component re-renders and picks up the state from getDerivedStateFromProps
+    setTimeout(() => this.setState({color: "purple"}), 2000)
+    console.log("after getDerivedStateFromProps")
+  }
   render() {
-      return (
+    return (
       <>
         <h2>Hi i'm {this.state.brand}</h2>
         model : {this.state.model}
-        <br/>color : {this.state.color}
-        <br/>year : {this.state.year}
+        <br />
+        color : {this.state.color}
+        <br />
+        year : {this.state.year}
       </>
     );
   }
 }
 export default CarParent;
-
-    // The getDerivedStateFromProps() method is called right before rendering the element(s) in the DOM. This is the natural place to set the state object based on the initial props.
-    
-    // It takes state as an argument, and returns an object with changes to the state.
-    
-    // The example below starts with the favorite color being "red", but the getDerivedStateFromProps() method updates the favorite color based on the favcol attribute:
-    
-    // note: Lifecycle method should be static: getDerivedStateFromProps
