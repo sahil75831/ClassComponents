204427a524a36ae23d6ac32993b59dca3a332a10
sahilsharma <sahilsharma75831@gmail.com>
2023-07-19 13:37:44 +0530

    first commit

--- ClassComp.jsx
+++ ClassComp.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React from "react";
 
 class Car extends React.Component {
-    render() {
-      return <h2>I am a Car!</h2>;
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: "sahil",
+      profile: "web developer",
+    };
   }
-export default Car
+  render() {
+    return (
+      <>
+        <h2>Hi i'm {this.state.name}</h2>
+        <i>i am a {this.state.profile}</i>
+      </>
+    );
+  }
+}
+export default Car;
+
+// If there is a constructor() function in your component, this function will be called when the component gets initiated.The constructor function is where you initiate the component's properties.
+
+// The constructor function is also where you "honor the inheritance of the parent component by including the super() statement" , which executes the parent component's constructor function, and your component has access to all the functions of the parent component (React.Component).
